8da34848226d8c20fad4623503e0389885007f5f
jnatishay78 <atishayjain070401@gmail.com>
2024-02-26 02:45:19 +0530

    updated

--- src/main/java/com/ecom/apii/config/AppConfig.java
+++ src/main/java/com/ecom/apii/config/AppConfig.java
@@ -41,10 +41,7 @@ public class AppConfig {
 						cfg.setAllowedOrigins(Arrays.asList(
 								
 								"http://localhost:3000", 
-								"http://localhost:4000",
-								"http://localhost:4200",
-								"https://shopwithzosh.vercel.app",
-								"https://ecommerce-angular-blue.vercel.app/"
+								"https://shopeefy.vercel.app/"
 								
 							)
 						);
